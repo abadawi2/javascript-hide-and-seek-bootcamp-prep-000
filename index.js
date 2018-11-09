143507ffcd4f4598324a6dec0a303f315c04a499
Alia B <abadawi@terpmail.umd.edu>
2018-11-09 17:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 function getFirstSelector(selector) {
-  return (document.querySelector(selector));
+  return document.querySelector(selector);
 }
 
-function nestedTarget(){
-  return document.getElementById("nested").querySelector(".target");
+function nestedTarget() {
+  const nestedTarget = document.getElementById("nested");
+  return nestedTarget.querySelector(".target");
 }
 
 function increaseRankBy(n) {
-  var lis= document.getElementById('app').querySelectorAll("ul.ranked-list li");
-  for(let i=0;lis.length>i;i++){
-  lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
-}
+  const ranks = document.getElementById("app").querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++)  {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML)+n;
+  }
 }
 
 function deepestChild(){
-  var lis =document.getElementById("grand-node").querySelectorAll("div");
-  var deepDefine;
-  for (let i = 0; lis.length-1 > i; i++){
-  deepDefine = lis[i].querySelector("div");
-  }
-  return deepDefine;
-}
+
+	let node = document.getElementById('grand-node');
+  	let nextNode = node.children[0];
+    while (nextNode) {
+      nextNode = node.children[0];
+    }
+    return (node);
+    }
